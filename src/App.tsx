4c8e865ebf9ae2ffa8828d720418f4c9ed032fd9
rafaeldquadros <103122923+rafaeldquadros@users.noreply.github.com>
2022-11-01 15:10:43 -0300

    Revert "Feat about us"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Routes from "./Routes";
-import { GlobalStyle } from "./Styles/Global";
 
 function App() {
-	return (
-		<>
-			<Routes />
-			<GlobalStyle />
-		</>
-	);
+    return (
+        <>
+            <Routes />
+        </>
+    );
 }
 
 export default App;
