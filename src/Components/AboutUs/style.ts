ddc179abb4bcad65ea144f54f6ff3be285101456
raphaeldamasceno1852 <rdamasceno1852@gmail.com>
2022-11-01 16:23:16 -0300

    feat:aboutUs created new branch, please delete the old branch

--- src/Components/AboutUs/style.ts
+++ src/Components/AboutUs/style.ts
@@ -1,25 +1,24 @@
 import styled from "styled-components";
 
 export const StyledDiv = styled.div`
-    
-    width: 95%;
-    margin: 0 auto;
+  width: 95%;
+  margin: 0 auto;
 
-
-    @media (min-width: 768px){box-sizing: border-box;
+  @media (min-width: 768px) {
+    box-sizing: border-box;
     height: 15.8rem;
-    padding: 100px;}
-
-`
+    padding: 100px;
+  }
+`;
 export const StyledTitle = styled.h3`
-    font-size: var(--main-subtitle-text-size);
-    font-weight: var(--main-title-text-weight);
-    color: var(--main-title-text-color);
-    line-height: 1.8rem;
-` 
+  font-size: var(--main-subtitle-text-size);
+  font-weight: var(--main-title-text-weight);
+  color: var(--main-title-text-color);
+  line-height: 1.8rem;
+`;
 export const StyledParagraph = styled.p`
-    height: 209px;
-    max-width: 1200px;
-    font-style: var(--main-subtitle-text-weight);
-    color: var(--main-subtitle-text-color)
-`
+  height: 209px;
+  max-width: 1200px;
+  font-style: var(--main-subtitle-text-weight);
+  color: var(--main-subtitle-text-color);
+`;
