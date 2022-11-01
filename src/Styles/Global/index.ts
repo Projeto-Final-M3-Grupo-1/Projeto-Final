f83ac30ef7360690deae0c1ea7076a0c8a6ebec2
gabs <gabs@pop-os.localdomain>
2022-11-01 11:26:32 -0300

    styles: added global styles

---
+++ src/Styles/Global/index.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --rose-900: #881337;
+        --rose-800: #9f1239;
+        --rose-700: #be123c;
+        --rose-600: #e11d48;
+        --rose-500: #f43f5e;
+        --rose-400: #fb7185;
+        --rose-300: #fda4af;
+        --rose-200: #fecdd3;
+        --rose-100: #ffe4e6;
+        --rose-50: #fff1f2;
+
+        --slate-900: #0f172a;
+        --slate-800: #1e293b;
+        --slate-700: #334155;
+        --slate-600: #475569;
+        --slate-500: #64748b;
+        --slate-400: #94a3b8;
+        --slate-300: #cbd5e1;
+        --slate-200: #e2e8f0;
+        --slate-100: #f1f5f9;
+        --slate-50: #f8fafc;
+    }
+`;
