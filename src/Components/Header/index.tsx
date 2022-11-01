11e3e6e3ee8855680730c6c31065a8b0a11ab36f
Roberto Jr <roberto-santos07@hotmail.com>
2022-11-01 16:14:52 -0300

    feat:Add Header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,1 +1,28 @@
-<></>;
+import React from "react";
+import { Link } from "react-router-dom";
+import { StyledHeader } from "./header";
+
+function Headers() {
+  return (
+    <StyledHeader>
+      <h2>Logo</h2>
+      <nav>
+        <Link className="links--menu_header" to="/home">
+          Início
+        </Link>
+        <Link className="links--menu_header" to="/sobre-nos">
+          Sobre nós
+        </Link>
+        <Link className="links--menu_header" to="/projetos">
+          Projetos
+        </Link>
+        <Link className="links--menu_header" to="/contato">
+          Contato
+        </Link>
+        <button type="button">Login</button>
+      </nav>
+    </StyledHeader>
+  );
+}
+
+export default Headers;
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,15 +1,9 @@
+import Headers from "../../Components/Header";
 export function Home() {
-    return (
-        <>
-            <h1>hi, i am home</h1>
-            {/* 
-                HEADER
-                MAIN
-                CARDS
-                SOBRE NOS
-                PROJETOS FINALIZADOS
-                FOOTER 
-             */}
-        </>
-    );
+  return (
+    <>
+      {<Headers />}
+      <h1>hi, i am home</h1>
+    </>
+  );
 }
