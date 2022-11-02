1918c82fef8ad94f630de94b736ebc5de8a0eea0
Roberto Jr <roberto-santos07@hotmail.com>
2022-11-02 14:57:10 -0300

    feat:Add mobile header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,33 +3,26 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ProjectsContext } from "../../Providers/ProjectsProvider";
 import { StyledHeader } from "./header";
-
+import { StyledLoginButton } from "../Button";
 function Headers() {
-  // const { menu, setMenu } = useContext(ProjectsContext);
-  // const handleClick = () => {
-  //     return !menu ? setMenu(true) : setMenu(false);
-  // };
   return (
     <StyledHeader>
       <h2>Logo</h2>
-      {/* {menu && (
-                <nav>
-                    <Link className="links--menu_header" to="/home">
-                        Início
-                    </Link>
-                    <Link className="links--menu_header" to="/sobre-nos">
-                        Sobre nós
-                    </Link>
-                    <Link className="links--menu_header" to="/projetos">
-                        Projetos
-                    </Link>
-                    <Link className="links--menu_header" to="/contato">
-                        Contato
-                    </Link>
-                </nav>
-            )}
-            <button onClick={handleClick}></button> */}
-      <button type="button">Login</button>
+      <nav>
+        <Link className="links--menu_header" to="/home">
+          Início
+        </Link>
+        <Link className="links--menu_header" to="/sobre-nos">
+          Sobre nós
+        </Link>
+        <Link className="links--menu_header" to="/projetos">
+          Projetos
+        </Link>
+        <Link className="links--menu_header" to="/contato">
+          Contato
+        </Link>
+      </nav>
+      <StyledLoginButton type="button">Login</StyledLoginButton>
     </StyledHeader>
   );
 }
