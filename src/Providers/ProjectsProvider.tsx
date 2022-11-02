6bbd9dbc82046bedffe12a08ad76183fc943c09e
Rafael Quadros <contatorafaelquadros@gmail.com>
2022-11-02 11:28:54 -0300

    create page for register dev

--- src/Providers/ProjectsProvider.tsx
+++ src/Providers/ProjectsProvider.tsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 import { ReactNode } from "react";
-import { array } from "yup";
+import { useNavigate } from "react-router-dom";
 import api from "../Services/api";
 
 interface IProjectsContext {
@@ -13,6 +13,7 @@ interface IProjectsContext {
     setShowModal: any;
     handleMenu: () => void;
     handleModal: () => void;
+    handleNavigate: any;
 }
 
 export const ProjectsContext = createContext<IProjectsContext>(
@@ -35,12 +36,18 @@ export const ProjectsProvider = ({ children }: IProjectChildren) => {
     const [menu, setMenu] = useState(false);
     const [showModal, setShowModal] = useState(false);
 
+    const navigate = useNavigate();
+
     const handleMenu = () => {
         return !menu ? setMenu(true) : setMenu(false);
     };
     const handleModal = () => {
         return !showModal ? setShowModal(true) : setShowModal(false);
     };
+    const handleNavigate = (route: string) => {
+        return navigate(route);
+    };
+
     const requestProjects = () => {
         api.get("/projects").then((res) => setProjects(res.data));
     };
@@ -56,6 +63,7 @@ export const ProjectsProvider = ({ children }: IProjectChildren) => {
                 setShowModal,
                 handleMenu,
                 handleModal,
+                handleNavigate,
             }}
         >
             {children}
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -4,16 +4,17 @@ import { Login } from "../Pages/Login";
 import { Home } from "../Pages/Home";
 import { DashboardDev } from "../Pages/DashboardDev";
 import { DashboardCompany } from "../Pages/DashboardCompany";
+import { RegisterDev } from "../Pages/RegisterDev";
 
 const Routers = () => {
     return (
         <Routes>
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            {/* <Route path="/registerdev" element={<RegisterDev/>} */}
             <Route path="/dashboarddev" element={<DashboardDev />} />
             <Route path="/dashboardcompany" element={<DashboardCompany />} />
             <Route path="*" element={<Navigate to="/home" />} />
+            <Route path="/registerdev" element={<RegisterDev />} />
         </Routes>
     );
 };
