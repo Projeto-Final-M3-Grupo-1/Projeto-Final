f8e0e4d5697ff9426f9cd7bc33e560561f6f8b53
Rafael Quadros <contatorafaelquadros@gmail.com>
2022-11-03 19:33:46 -0300

    feat/perfilUser: add modal perfil dev

--- src/Components/Dashboards/DashboardDev/index.tsx
+++ src/Components/Dashboards/DashboardDev/index.tsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { ProjectsContext } from "../../../Providers/ProjectsProvider";
+import { PerfilDev } from "../../Modal/ModalPerfilDev";
 export const DashboardDev = () => {
+    const { handleModal, showModal } = useContext(ProjectsContext);
     return (
         <>
-            <h1>eu sou a das do dev</h1>
+            {showModal && <PerfilDev />}
+            <button onClick={handleModal}>perfil</button>
         </>
     );
 };
