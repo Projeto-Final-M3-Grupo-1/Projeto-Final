1a8b89bcfd2c72cf4a2215a3996693183a8548e7
brunobbittencourt <bbittencourt@outlook.com.br>
2022-11-03 14:53:13 -0300

    fix: texts registerDev and registerOng

--- src/Pages/RegisterOng/index.tsx
+++ src/Pages/RegisterOng/index.tsx
@@ -18,32 +18,22 @@ export const RegisterOng = () => {
         <StyledTextPageOng>
           <h1>Gostaria de ter um site desenvolvido pela nossa equipe?</h1>
           <p>
-            Nossa plataforma trabalha em conjunto com ONGs e Projetos Sociais
-            que já estão em funcionamento e com cadastros validos no Governo
-            Federal, realizamos de forma gratuita o desenvolvimento de sites
-            totalmente únicos e direcionados as necessidades de cada ONG em
-            especial. Para entrar no processo de seleção você precisa ter um
-            cadastro na nossa plataforma e lembrar se que na hora de se
-            registrar todos os campos são obrigatórios. Após o cadastro você
-            marcará uma data para conversar com um de nossos Líderes para que
-            possamos entender o que você precisa, em questão de funcionalidades
-            para seu site e também para avaliarmos a viabilidade do seu projeto.
-            Depois da entrevista concluida e de todo planejamento concluido, seu
-            projeto entrará em uma lista de espera e irá para sorteio. Todos os
-            sorteios são feitos ao vivo pelo youtube e instagram trazendo
-            transparência para nossa plataforma. O numero de sorteados pode
-            variar dependendo da quantidade de projetos que estarão em andamento
-            e a quantidade de voluntários. Não haverá uma data fixa para o
-            sorteio de projetos já que para cada situação o tempo de
-            desenvolvimento pode variar. Dito isto preencha todos os campos de
-            registro, e após criar sua conta é muito importante criar postagens
-            dentro de seu perfil para que os desenvolvedores voluntários se
-            interessem em ajudar seu projeto, poste fotos de seus eventos já
-            realizados, as datas de eventos futuros para que os desenvolvedores
-            possam acompanhar a movimentação do seu projeto e até mesmo ir
-            conhecer. Isto ajudará de forma efetiva no momento da formação de
-            equipes para seu projeto, e quanto mais voluntários se interessarem
-            em lhe ajudar mais facil será o desenvolvimento!
+            Nossa plataforma trabalha em conjunto com organizações filantrópicas
+            que estejam em pleno funcionamento (ONGs e Projetos Sociais) e
+            realizamos, de forma gratuita, o desenvolvimento de sites únicos e
+            direcionados às necessidades de cada instituição parceira.
+          </p>
+          <p>
+            Desta forma, nossa plataforma faz a conexão entre a instituição
+            filantrópica e o desenvolvedor, para que o seu projeto seja
+            desenvolvido de forma dinâmica, por meio das melhores práticas e com
+            um resultado de excelência.
+          </p>
+          <p>
+            Para ingressar no processo de seleção como instituição filantrópica
+            é muito simples, basta seguir alguns passos: a) realizar um cadastro
+            na nossa plataforma; b) agendar um horário para conversar com a
+            nossa equipe técnica e passar as necessidades da sua organização.
           </p>
         </StyledTextPageOng>
         <StyledButtonCta>Quero fazer o bem</StyledButtonCta>
