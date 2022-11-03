665a8a30d58dde2a156797c25e3bf933a65af98c
Rafael Quadros <contatorafaelquadros@gmail.com>
2022-11-03 12:47:32 -0300

    fix: scope problem on styled buttons

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,64 +1,60 @@
 import styled from "styled-components";
 
 export const StyledNewProjectButton = styled.button`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0px;
+    gap: 10px;
 
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 0px;
-  gap: 10px;
+    width: 149px;
+    height: 150px;
 
-  width: 149px;
-  height: 150px;
-
-
-  background: var(--button-background-color);
-  color: var(--button-text-color);
-  font-size: var(--button-text-size);
-  font-weight: var(--button-text-weight);
-  border-radius: 50%;
-  border: 8px;
-  border: 8px solid var(--white);
-  cursor: pointer;
-
+    background: var(--button-background-color);
+    color: var(--button-text-color);
+    font-size: var(--button-text-size);
+    font-weight: var(--button-text-weight);
+    border-radius: 50%;
+    border: 8px;
+    border: 8px solid var(--white);
+    cursor: pointer;
 
     &:hover {
         background: var(--button-background-hover-color);
         transition: var(--button-hover-transition);
     }
 
-    @media(max-width: 400px){
-        margin-left: 40%; 
+    @media (max-width: 400px) {
+        margin-left: 40%;
     }
-
+`;
 export const StyledLoginButton = styled.button`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 10px 28px;
-  gap: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 28px;
+    gap: 10px;
 
-  width: 99px;
-  height: 40px;
+    width: 99px;
+    height: 40px;
 
-  background: var(--button-background-color);
-  color: var(--button-text-color);
-  font-size: var(--button-text-size);
-  font-weight: var(--button-text-weight);
-  border-radius: 8px;
-  border: none;
-  cursor: pointer;
+    background: var(--button-background-color);
+    color: var(--button-text-color);
+    font-size: var(--button-text-size);
+    font-weight: var(--button-text-weight);
+    border-radius: 8px;
+    border: none;
+    cursor: pointer;
 
-  &:hover {
-    background: var(--button-background-hover-color);
-    transition: var(--button-hover-transition);
-  }
+    &:hover {
+        background: var(--button-background-hover-color);
+        transition: var(--button-hover-transition);
+    }
 `;
 
 export const StyledButtonCta = styled.button`
-
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -82,32 +78,32 @@ export const StyledButtonCta = styled.button`
         transition: var(--button-hover-transition);
     }
 
-    @media(max-width: 600px){
+    @media (max-width: 600px) {
         width: 100%;
     }
 `;
 
 export const StyledButtonCadastro = styled.button`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 10px 28px;
-  gap: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 28px;
+    gap: 10px;
 
-  width: 205px;
-  height: 40px;
+    width: 205px;
+    height: 40px;
 
-  background: var(--button-background-color);
-  color: var(--button-text-color);
-  font-size: var(--button-text-size);
-  font-weight: var(--button-text-weight);
-  border-radius: 8px;
-  border: none;
-  cursor: pointer;
+    background: var(--button-background-color);
+    color: var(--button-text-color);
+    font-size: var(--button-text-size);
+    font-weight: var(--button-text-weight);
+    border-radius: 8px;
+    border: none;
+    cursor: pointer;
 
-  &:hover {
-    background: var(--button-background-hover-color);
-    transition: var(--button-hover-transition);
-  }
+    &:hover {
+        background: var(--button-background-hover-color);
+        transition: var(--button-hover-transition);
+    }
 `;
