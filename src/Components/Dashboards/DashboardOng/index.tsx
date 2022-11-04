52fdd0cdd6a5e452d634d59bdc873668e234b277
Rafael Quadros <contatorafaelquadros@gmail.com>
2022-11-04 08:33:46 -0300

    feat/headersDash: create header from dash and fix problem on register

--- src/Components/Dashboards/DashboardOng/index.tsx
+++ src/Components/Dashboards/DashboardOng/index.tsx
@@ -1,12 +1,23 @@
+import { useContext } from "react";
+
+import { ProjectsContext } from "../../../Providers/ProjectsProvider";
+
 import MenuAsideOng from "../../Asides/AsideOng";
-import Headers from "../../Header";
+import { StyledLoginButton } from "../../Button";
+import { StyledHeader } from "../../Header/header";
 
 export const DashboardOng = () => {
-  return (
-    <>
-      <Headers />
-      <MenuAsideOng />
-      <h1>eu sou a das do ong</h1>
-    </>
-  );
+    const { handleNavigate } = useContext(ProjectsContext);
+    return (
+        <>
+            <StyledHeader>
+                <h2>Logo</h2>
+                <StyledLoginButton onClick={() => handleNavigate("/home")}>
+                    Logout
+                </StyledLoginButton>
+            </StyledHeader>
+            <MenuAsideOng />
+            <h1>eu sou a das do ong</h1>
+        </>
+    );
 };
