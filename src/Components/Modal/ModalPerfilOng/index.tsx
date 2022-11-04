2e7a86efcfd7d8b92c368436d6da3e599e546a29
Rafael Quadros <contatorafaelquadros@gmail.com>
2022-11-04 20:37:02 -0300

    feat/modalPerfilOng: fix map from user projects

--- src/Components/Modal/ModalPerfilOng/index.tsx
+++ src/Components/Modal/ModalPerfilOng/index.tsx
@@ -8,125 +8,144 @@ import { StyledButtonCadastro } from "../../Button";
 import { ButtonCloseModal } from "../../Button/ButtonCloseModal";
 import { StyledBoxModal } from "../ModalLogin/style";
 import {
-  StyledModalBody,
-  StyledOngDetails,
-  StyledProjectsRequests,
-  StyledProjectDetails,
-  StyledInfo,
-  StyledDescription,
-  StyledContent,
-  StyledButtons,
+    StyledModalBody,
+    StyledOngDetails,
+    StyledProjectsRequests,
+    StyledProjectDetails,
+    StyledInfo,
+    StyledDescription,
+    StyledContent,
+    StyledButtons,
 } from "./style";
 
 interface iState {
-  length: any;
-  map(arg0: (elem: any) => void): import("react").ReactNode;
-  projects:any;
-  title: string;
-  description: string;
-  id: number;
-  user: number;
-  setProjects: any;
-
+    length: any;
+    map(arg0: (elem: any) => void): import("react").ReactNode;
+    projects: any;
+    title: string;
+    description: string;
+    id: number;
+    user: number;
+    setProjects: any;
 }
 
 export const ModalPerfilOng = () => {
-  const { handleModal, handleNavigate } = useContext(ProjectsContext);
-  const { dataUser } = useContext(AuthContext);
-  const { user } = useContext(UserContext);
-  const [projects, setProjetcts] = useState([] as unknown as iState);
+    const { handleModal, handleNavigate } = useContext(ProjectsContext);
+    const { dataUser } = useContext(AuthContext);
+    const { user } = useContext(UserContext);
+    const [projects, setProjetcts] = useState([] as unknown as iState);
 
-  useEffect(() => {
-    const getProject = () => {
-      api
-        .get(`/projects`)
-        .then((res) => {
-          setProjetcts(res.data);
-        })
-        .catch((error) => console.log(error));
-    };
-    getProject();
-  }, []);
+    useEffect(() => {
+        const getProject = () => {
+            api.get(`/projects`)
+                .then((res) => {
+                    setProjetcts(res.data);
+                })
+                .catch((error) => console.log(error));
+        };
+        getProject();
+    }, []);
 
-  console.log(projects);
+    console.log(projects);
 
-  return (
-    <>
-      <StyledBoxModal>
-        <StyledModalBody>
-          <ButtonCloseModal callback={handleModal} />
+    return (
+        <>
+            <StyledBoxModal>
+                <StyledModalBody>
+                    <ButtonCloseModal callback={handleModal} />
 
-          <StyledContent>
-            <StyledOngDetails>
-              <div className="profile">
-                <caption>
-                  <img src={dataUser.fotoDePerfil} alt="" />
-                </caption>
-                <div className="details">
-                  <h3>{dataUser.razaoSocial}</h3>
-                  <p>ONG</p>
-                </div>
-              </div>
-              <StyledProjectDetails>
-                <StyledInfo>
-                  <p className="label">Nome do projeto</p>
-                  <input className="info" value={dataUser.razaoSocial} />
-                </StyledInfo>
+                    <StyledContent>
+                        <StyledOngDetails>
+                            <div className="profile">
+                                <caption>
+                                    <img src={dataUser.fotoDePerfil} alt="" />
+                                </caption>
+                                <div className="details">
+                                    <h3>{dataUser.razaoSocial}</h3>
+                                    <p>ONG</p>
+                                </div>
+                            </div>
+                            <StyledProjectDetails>
+                                <StyledInfo>
+                                    <p className="label">Nome do projeto</p>
+                                    <input
+                                        className="info"
+                                        value={dataUser.razaoSocial}
+                                    />
+                                </StyledInfo>
 
-                <StyledInfo>
-                  <p className="label">Razão Social</p>
-                  <input className="info" value={dataUser.razaoSocial} />
-                </StyledInfo>
+                                <StyledInfo>
+                                    <p className="label">Razão Social</p>
+                                    <input
+                                        className="info"
+                                        value={dataUser.razaoSocial}
+                                    />
+                                </StyledInfo>
 
-                <StyledInfo>
-                  <p className="label">CNPJ</p>
-                  <input className="info" value={dataUser.cnpj} />
-                </StyledInfo>
+                                <StyledInfo>
+                                    <p className="label">CNPJ</p>
+                                    <input
+                                        className="info"
+                                        value={dataUser.cnpj}
+                                    />
+                                </StyledInfo>
 
-                <StyledDescription>
-                  <p>O que fazemos</p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Etiam eu lorem sit amet odio ullamcorper pretium.
-                    Suspendisse sed iaculis massa. Vivamus varius semper
-                    posuere. Proin aliquet vel est id ultrices. Vestibulum quis
-                    pharetra lectus. Praesent vel nulla arcu. Proin eget sodales
-                    odio.
-                  </p>
-                </StyledDescription>
-              </StyledProjectDetails>
-            </StyledOngDetails>
+                                <StyledDescription>
+                                    <p>O que fazemos</p>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit. Etiam eu lorem sit amet
+                                        odio ullamcorper pretium. Suspendisse
+                                        sed iaculis massa. Vivamus varius semper
+                                        posuere. Proin aliquet vel est id
+                                        ultrices. Vestibulum quis pharetra
+                                        lectus. Praesent vel nulla arcu. Proin
+                                        eget sodales odio.
+                                    </p>
+                                </StyledDescription>
+                            </StyledProjectDetails>
+                        </StyledOngDetails>
 
-            <StyledProjectsRequests>
-              <h3 className="title">Solicitações do Projeto</h3>
+                        <StyledProjectsRequests>
+                            <h3 className="title">Solicitações do Projeto</h3>
 
-              <div className="projectInfo">
-                <h3 className="name">{dataUser.razaoSocial}</h3>
-               <ul>
-               {projects.length ?
-               (
-                 projects.map(elem => 
-                  console.log(elem)
+                            <div className="projectInfo">
+                                <h3 className="name">{dataUser.razaoSocial}</h3>
+                                {projects.length ? (
+                                    projects.map((element) => {
+                                        if (element.ongId == dataUser.id) {
+                                            return (
+                                                <>
+                                                    <p className="title">
+                                                        {element.title}
+                                                    </p>
+                                                    <p className="description">
+                                                        {element.description}
+                                                    </p>
+                                                </>
+                                            );
+                                        }
+                                    })
+                                ) : (
+                                    <h1>Não Rolou</h1>
+                                )}
 
-               )):(
-                <h1>Nada encontrado</h1>
-               )
-                  
-                )}
-               </ul>
-
-                <StyledButtonCadastro>Ver mais</StyledButtonCadastro>
-              </div>
-            </StyledProjectsRequests>
-            <StyledButtons>
-              <StyledButtonCadastro>Salvar</StyledButtonCadastro>
-              <StyledButtonCadastro onClick={() => handleNavigate("/home")}>
-                Logout
-              </StyledButtonCadastro>
-            </StyledButtons>
-          </StyledContent>
-        </StyledModalBody>
-      </StyledBoxModal>
-    </>
-  );
+                                <StyledButtonCadastro>
+                                    Ver mais
+                                </StyledButtonCadastro>
+                            </div>
+                        </StyledProjectsRequests>
+                        <StyledButtons>
+                            <StyledButtonCadastro>Salvar</StyledButtonCadastro>
+                            <StyledButtonCadastro
+                                onClick={() => handleNavigate("/home")}
+                            >
+                                Logout
+                            </StyledButtonCadastro>
+                        </StyledButtons>
+                    </StyledContent>
+                </StyledModalBody>
+            </StyledBoxModal>
+        </>
+    );
 };
