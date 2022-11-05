a65756d2914f0e941933d8b6ba82b070c3ab4987
Gabs Ghidini <gabsghidini@gmail.com>
2022-11-05 13:13:24 -0300

    style: mobile now has dropdown menu

--- src/Providers/AuthContext.tsx
+++ src/Providers/AuthContext.tsx
@@ -2,35 +2,34 @@ import { createContext, ReactNode, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../Services/api";
 interface IAuthContext {
-    loadingUser: any;
-    dataUser: any;
-    setDataUser: any;
+	loadingUser: any;
+	dataUser: any;
+	setDataUser: any;
 }
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
 interface IAuthChildren {
-    children: ReactNode;
+	children: ReactNode;
 }
 
 export const AuthProvider = ({ children }: IAuthChildren) => {
-    const navigate = useNavigate();
-    const [dataUser, setDataUser] = useState({});
-    const loadingUser = () => {
-        api.get(`/users/${localStorage.userId}`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.token}`,
-            },
-        })
-            .then((res) => {
-                console.log(res);
-                setDataUser(res.data);
-            })
-            .catch(() => navigate("/home"));
-    };
-    return (
-        <AuthContext.Provider value={{ loadingUser, dataUser, setDataUser }}>
-            {children}
-        </AuthContext.Provider>
-    );
+	const navigate = useNavigate();
+	const [dataUser, setDataUser] = useState({});
+	const loadingUser = () => {
+		api.get(`/users/${localStorage.userId}`, {
+			headers: {
+				Authorization: `Bearer ${localStorage.token}`,
+			},
+		})
+			.then((res) => {
+				setDataUser(res.data);
+			})
+			.catch(() => navigate("/home"));
+	};
+	return (
+		<AuthContext.Provider value={{ loadingUser, dataUser, setDataUser }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
