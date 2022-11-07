8efced031092c5efda498d5a2b5f8364710fbf56
Gabs Ghidini <gabsghidini@gmail.com>
2022-11-07 13:08:47 -0300

    Feat: modal created

--- src/Components/Modal/ModalNovaPublicacao/index.tsx
+++ src/Components/Modal/ModalNovaPublicacao/index.tsx
@@ -1,5 +1,56 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../../Providers/AuthContext";
+import * as S from "./styles";
+
 const ModalNovaPublicacao = () => {
-	return <div>ModalNovaPublicacao</div>;
+	const { dataUser, loadingUser } = useContext(AuthContext);
+
+	useEffect(() => {
+		loadingUser();
+	}, []);
+
+	return (
+		<S.Background>
+			<S.Container>
+				<S.Content>
+					<S.UserContainer>
+						<S.UserImage
+							src={dataUser.fotoDePerfil}
+							alt="Foto de Perfil"
+						/>
+						<S.UserInfo>
+							<S.UserName>{dataUser.nome}</S.UserName>
+							<S.UserType>
+								{dataUser.typeUser === "dev"
+									? "Desenvolvedor Voluntário"
+									: "Loading..."}
+							</S.UserType>
+						</S.UserInfo>
+					</S.UserContainer>
+					<S.Form>
+						<S.Legend>Título</S.Legend>
+						<S.Input
+							type="text"
+							placeholder="ONG Do Bem se Prepara para lançar manual de ecologia"
+						/>
+						<S.Legend>Imagem URL</S.Legend>
+						<S.Input
+							type="text"
+							placeholder="https://i.imgur.com/removed.png"
+						/>
+						<S.Legend>Site URL</S.Legend>
+						<S.Input
+							type="text"
+							placeholder="https://www.ongdobem.com.br"
+						/>
+						<S.Legend>Descrição</S.Legend>
+						<S.Description placeholder="A ONG Do Bem se prepara para lançar um manual de ecologia para crianças, o manual será lançado em breve e será disponibilizado gratuitamente para todos os interessados." />
+						<S.Button>Salvar</S.Button>
+					</S.Form>
+				</S.Content>
+			</S.Container>
+		</S.Background>
+	);
 };
 
 export default ModalNovaPublicacao;
--- src/Components/Modal/ModalNovaPublicacao/styles.ts
+++ src/Components/Modal/ModalNovaPublicacao/styles.ts
@@ -1,13 +1,126 @@
 import styled from "styled-components";
 
-export const Modal = styled.div`
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background-color: rgba(var(--slate-800), 0.8);
+export const Background = styled.div`
+	background-color: rgba(30, 41, 59, 0.8);
+	height: 100vh;
+	width: 100vw;
 	display: flex;
 	justify-content: center;
 	align-items: center;
+`;
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	max-width: 1200px;
+	height: auto;
+	min-height: 840px;
+	background-color: var(--white);
+	border-radius: 8px;
+`;
+
+export const Content = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+	width: 80%;
+	max-width: 996px;
+`;
+
+export const UserContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: flex-start;
+
+	width: 100%;
+`;
+
+export const UserImage = styled.img`
+	width: 100px;
+	height: 100px;
+	border-radius: 50%;
+	border: 4px solid var(--rose-600);
+`;
+
+export const UserInfo = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-left: 20px;
+`;
+
+export const UserName = styled.h1`
+	font-size: 24px;
+	color: var(--slate-800);
+	font-weight: 600;
+`;
+
+export const UserType = styled.h2`
+	font-size: 16px;
+	color: var(--slate-800);
+	font-weight: 500;
+`;
+
+export const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+
+	width: 100%;
+	max-width: 996px;
+	height: 100%;
+
+	gap: 0.5rem;
+	margin-top: 1rem;
+`;
+
+export const Input = styled.input`
+	width: 100%;
+	align-self: center;
+
+	height: 45px;
+
+	border: 2px solid var(--rose-600);
+	border-radius: 4px;
+
+	&:focus {
+		outline: none;
+	}
+`;
+
+export const Legend = styled.legend`
+	font-size: 24px;
+	color: var(--slate-800);
+	font-weight: 500;
+`;
+
+export const Button = styled.button`
+	width: 100%;
+	align-self: center;
+	height: 40px;
+	margin-top: 24px;
+	background-color: var(--rose-600);
+	border: none;
+	border-radius: 4px;
+	color: var(--white);
+	cursor: pointer;
+`;
+
+export const Description = styled.textarea`
+	display: flex;
+	align-self: center;
+	width: 100%;
+	resize: none;
+	height: 200px;
+
+	border: 2px solid var(--rose-600);
+	border-radius: 4px;
+
+	&:focus {
+		outline: none;
+	}
 `;
