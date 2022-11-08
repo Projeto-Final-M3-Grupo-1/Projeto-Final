edeeed56fd8228b9b94978da7db4c40e00deb69d
Roberto Jr <roberto-santos07@hotmail.com>
2022-11-07 23:04:47 -0300

    feat/ModalCreateProject: Add modal create projects

--- src/Components/Dashboards/DashboardOng/index.tsx
+++ src/Components/Dashboards/DashboardOng/index.tsx
@@ -5,21 +5,24 @@ import { UserContext } from "../../../Providers/UserProvider";
 
 import MenuAsideOng from "../../Asides/AsideOng";
 import HeaderDashboard from "../../HeaderDashboard";
+import ModalCreateProject from "../../Modal/ModalAddProject";
 import { ModalPerfilOng } from "../../Modal/ModalPerfilOng";
 import Publications from "../../Publication";
 
 export const DashboardOng = () => {
-    const { showModal, handleNavigate } = useContext(ProjectsContext);
-    const { renderPublications } = useContext(UserContext);
+  const { showModal, handleNavigate, showProject } =
+    useContext(ProjectsContext);
+  const { renderPublications } = useContext(UserContext);
 
-    useEffect(() => {
-        renderPublications();
-    }, []);
-    return (
-        <>
-            <HeaderDashboard />
-            {showModal && <ModalPerfilOng />}
-            <Publications></Publications>
-        </>
-    );
+  useEffect(() => {
+    renderPublications();
+  }, []);
+  return (
+    <>
+      <HeaderDashboard />
+      {showModal && <ModalPerfilOng />}
+      {showProject && <ModalCreateProject />}
+      <Publications></Publications>
+    </>
+  );
 };
--- src/Providers/ProjectsProvider.tsx
+++ src/Providers/ProjectsProvider.tsx
@@ -2,77 +2,105 @@ import { createContext, useState } from "react";
 import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 import { animateScroll as scroll } from "react-scroll";
+import { toast } from "react-toastify";
 import api from "../Services/api";
 
 interface IProjectsContext {
-    projects: any;
-    setProjects: any;
-    requestProjects: () => void;
-    menu: boolean;
-    setMenu: any;
-    showModal: boolean;
-    setShowModal: any;
-    modalHome: boolean;
-    setModalHome:React.Dispatch<React.SetStateAction<boolean>>;
-    handleMenu: () => void;
-    handleModal: () => void;
-    handleNavigate: any;
-    scrollToTop: () => void;
+  projects: any;
+  setProjects: any;
+  requestProjects: () => void;
+  menu: boolean;
+  setMenu: any;
+  showModal: boolean;
+  setShowModal: any;
+  modalHome: boolean;
+  setModalHome: React.Dispatch<React.SetStateAction<boolean>>;
+  handleMenu: () => void;
+  handleModal: () => void;
+  handleNavigate: any;
+  createProjects: any;
+  showProject: any;
+  HandleModalProject: () => void;
+  scrollToTop: () => void;
 }
 
 export const ProjectsContext = createContext<IProjectsContext>(
-    {} as IProjectsContext
+  {} as IProjectsContext
 );
 
 interface IProjectChildren {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const ProjectsProvider = ({ children }: IProjectChildren) => {
-    const [projects, setProjects] = useState([] as any);
-    const [menu, setMenu] = useState(false);
-    const [showModal, setShowModal] = useState(false);
-    const [modalHome, setModalHome] = useState(false)
+  const [projects, setProjects] = useState([] as any);
+  const [menu, setMenu] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalHome, setModalHome] = useState(false);
+  const [showProject, setShowProjects] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleMenu = () => {
-        return !menu ? setMenu(true) : setMenu(false);
-    };
-    const handleModal = () => {
-        return !showModal ? setShowModal(true) : setShowModal(false);
-    };
-    const handleNavigate = (route: string) => {
-        return navigate(route);
-    };
+  const handleMenu = () => {
+    return !menu ? setMenu(true) : setMenu(false);
+  };
+  const handleModal = () => {
+    return !showModal ? setShowModal(true) : setShowModal(false);
+  };
 
-    const requestProjects = () => {
-        api.get("/projects").then((res) => setProjects(res.data));
-    };
+  const HandleModalProject = () => {
+    return !showProject ? setShowProjects(true) : setShowProjects(false);
+  };
+  const handleNavigate = (route: string) => {
+    return navigate(route);
+  };
 
-    const scrollToTop = () => {
-        scroll.scrollToTop();
-    };
+  const requestProjects = () => {
+    api.get("/projects").then((res) => setProjects(res.data));
+  };
 
-    return (
-        <ProjectsContext.Provider
-            value={{
-                scrollToTop,
-                requestProjects,
-                projects,
-                setProjects,
-                menu,
-                setMenu,
-                showModal,
-                setShowModal,
-                handleMenu,
-                handleModal,
-                handleNavigate,
-                modalHome, 
-                setModalHome
-            }}
-        >
-            {children}
-        </ProjectsContext.Provider>
-    );
+  const createProjects = (data: any) => {
+    data.userId = localStorage.userId;
+    data.ongId = localStorage.userId;
+    console.log(data);
+    api
+      .post("/pendings", data, {
+        headers: {
+          Authorization: `Bearer ${localStorage.token}`,
+        },
+      })
+      .then(() => {
+        setShowProjects(false);
+        toast.success("Projeto cadastrado com sucesso!");
+      });
+  };
+
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  return (
+    <ProjectsContext.Provider
+      value={{
+        scrollToTop,
+        requestProjects,
+        projects,
+        setProjects,
+        menu,
+        setMenu,
+        showModal,
+        setShowModal,
+        handleMenu,
+        handleModal,
+        handleNavigate,
+        modalHome,
+        setModalHome,
+        HandleModalProject,
+        createProjects,
+        showProject,
+      }}
+    >
+      {children}
+    </ProjectsContext.Provider>
+  );
 };
