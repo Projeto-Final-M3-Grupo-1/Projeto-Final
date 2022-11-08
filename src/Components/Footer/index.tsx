977bc5db0a4fbc70894f081e38428f3e92c2c672
Gabs Ghidini <gabsghidini@gmail.com>
2022-11-08 14:32:15 -0300

    fix: logo is now present on on pages

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -4,38 +4,38 @@ import { TbArrowBigTop } from "react-icons/tb";
 import { StyledFooter } from "./style";
 
 import {
-  AiOutlineFacebook,
-  AiOutlineInstagram,
-  AiOutlineLinkedin,
+	AiOutlineFacebook,
+	AiOutlineInstagram,
+	AiOutlineLinkedin,
 } from "react-icons/ai";
 import { useParams } from "react-router-dom";
 
 export const Footer = () => {
-  const { scrollToTop } = useContext(ProjectsContext);
+	const { scrollToTop } = useContext(ProjectsContext);
 
-  return (
-    <StyledFooter>
-      <div>
-        <h5>Contatos</h5>
-        <ul>
-          <li>
-            <AiOutlineInstagram />
-            <p>@DeVoluntario</p>
-          </li>
-          <li>
-            <AiOutlineLinkedin />
-            <p>/DeVoluntario</p>
-          </li>
-          <li>
-            <AiOutlineFacebook />
-            <p>/DeVoluntario</p>
-          </li>
-          <TbArrowBigTop className="scrolltop" onClick={scrollToTop}>
-            Topo
-          </TbArrowBigTop>
-        </ul>
-      </div>
-      <button>Copyright - Todos os Direitos Reservados ao Grupo 1</button>
-    </StyledFooter>
-  );
+	return (
+		<StyledFooter>
+			<div>
+				<h5>Contatos</h5>
+				<ul>
+					<li>
+						<AiOutlineInstagram />
+						<p>@DeVolunteer</p>
+					</li>
+					<li>
+						<AiOutlineLinkedin />
+						<p>/DeVolunteer</p>
+					</li>
+					<li>
+						<AiOutlineFacebook />
+						<p>/DeVolunteer</p>
+					</li>
+					<TbArrowBigTop className="scrolltop" onClick={scrollToTop}>
+						Topo
+					</TbArrowBigTop>
+				</ul>
+			</div>
+			<button>Copyright - Todos os Direitos Reservados ao Grupo 1</button>
+		</StyledFooter>
+	);
 };
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -4,7 +4,6 @@ import { Home } from "../Pages/Home";
 import { Dashboard } from "../Pages/DashboardCompany";
 import { RegisterDev } from "../Pages/RegisterDev";
 import { RegisterOng } from "../Pages/RegisterOng";
-import Test from "../Components/Modal/ModalNovaPublicacao";
 
 const Routers = () => {
 	return (
