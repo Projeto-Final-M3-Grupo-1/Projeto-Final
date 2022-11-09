a3b5dbe1c0be69bdf16229f63818ed8fbc471bab
Rafael Quadros <contatorafaelquadros@gmail.com>
2022-11-09 13:43:27 -0300

    feat/manageProject: att typescript

--- src/Components/Modal/ModalCreateTask/index.tsx
+++ src/Components/Modal/ModalCreateTask/index.tsx
@@ -12,96 +12,68 @@ import { TextArea } from "../../TextArea";
 import { StyledBoxModalCreateTask } from "./styles";
 
 interface iCreateTask {
-<<<<<<< HEAD
-  open: boolean;
-  handleClose: () => void;
-=======
->>>>>>> 80c358fe1584e78d786c976ada63b257fa96cfce
-  projectId: number;
+    open: boolean;
+    handleClose: () => void;
+
+    projectId: number;
 }
 
 interface iFormTask {
-  title: string;
-  content: string;
-<<<<<<< HEAD
-}
+    title: string;
+    content: string;
 
-export const CreateTask = ({ open, handleClose, projectId }: iCreateTask) => {
-  const { onSubmitCreateTask } = useContext(UserContext);
-  const { handleModal } = useContext(ProjectsContext);
-=======
-  projectId: number;
+    projectId: number;
 }
 
 export const CreateTask = () => {
-  const { onSubmitCreateTask } = useContext(UserContext);
-  const { handleCreateTask } = useContext(ProjectsContext);
->>>>>>> 80c358fe1584e78d786c976ada63b257fa96cfce
-  const {
-    handleSubmit,
-    register,
-    formState: {
-      errors: { title, content },
-    },
-  } = useForm<iFormTask>({
-    resolver: yupResolver(schemaCreateTask),
-  });
-  return (
-    <StyledBoxModalCreateTask>
-<<<<<<< HEAD
-      <StyledForm
-        onSubmit={handleSubmit((data) =>
-          onSubmitCreateTask({ ...data, projectId })
-        )}
-      >
-        <ButtonCloseModal callback={handleModal} />
-=======
-      <StyledForm onSubmit={handleSubmit(onSubmitCreateTask)}>
-        <ButtonCloseModal callback={handleCreateTask} />
->>>>>>> 80c358fe1584e78d786c976ada63b257fa96cfce
-        <div className="containerModal">
-          <figure>
-            <img
-              src="http://www.abo-sc.org.br/wp-content/uploads/2017/06/img-perfil-masc2.png"
-              alt=""
-            />
-          </figure>
-          <div className="containerNomeFuncao">
-            <h2>João Silva</h2>
-            <p>Tech Leader</p>
-          </div>
-        </div>
-<<<<<<< HEAD
+    const { onSubmitCreateTask } = useContext(UserContext);
+    const { handleCreateTask } = useContext(ProjectsContext);
 
-        <InputAndLabel
-          textLabel="Título"
-          name="title"
-          placeholder="Título do seu Projeto"
-          type="text"
-          register={register}
-          error={title?.message}
-        />
-        <TextArea
-          textLabel="Descrição"
-          name="content"
-          placeholder="Faça um resumo da tarefa a ser realizada"
-          type="text"
-          error={content?.message}
-          register={register}
-        />
-=======
-        <InputAndLabel
-          textLabel="Nome da Task"
-          name="title"
-          placeholder="Digite o nome da task"
-          type="text"
-          register={register}
-          error={title?.message}
-        />
+    const {
+        handleSubmit,
+        register,
+        formState: {
+            errors: { title, content },
+        },
+    } = useForm<iFormTask>({
+        resolver: yupResolver(schemaCreateTask),
+    });
 
->>>>>>> 80c358fe1584e78d786c976ada63b257fa96cfce
-        <StyledLoginButton type="submit">Salvar</StyledLoginButton>
-      </StyledForm>
-    </StyledBoxModalCreateTask>
-  );
+    return (
+        <StyledBoxModalCreateTask>
+            <StyledForm onSubmit={handleSubmit(onSubmitCreateTask)}>
+                <ButtonCloseModal callback={handleCreateTask} />
+                <div className="containerModal">
+                    <figure>
+                        <img
+                            src="http://www.abo-sc.org.br/wp-content/uploads/2017/06/img-perfil-masc2.png"
+                            alt=""
+                        />
+                    </figure>
+                    <div className="containerNomeFuncao">
+                        <h2>João Silva</h2>
+                        <p>Tech Leader</p>
+                    </div>
+                </div>
+
+                <InputAndLabel
+                    textLabel="Título"
+                    name="title"
+                    placeholder="Título do seu Projeto"
+                    type="text"
+                    register={register}
+                    error={title?.message}
+                />
+                <TextArea
+                    textLabel="Descrição"
+                    name="content"
+                    placeholder="Faça um resumo da tarefa a ser realizada"
+                    type="text"
+                    error={content?.message}
+                    register={register}
+                />
+                <StyledLoginButton type="submit">Salvar</StyledLoginButton>
+            </StyledForm>
+        </StyledBoxModalCreateTask>
+    );
 };
