bd7859f7172707d57601546d507855bd4301ef27
brunobbittencourt <bbittencourt@outlook.com.br>
2022-11-09 10:45:48 -0300

    feat: TS AuthContext done

--- src/Providers/AuthContext.tsx
+++ src/Providers/AuthContext.tsx
@@ -1,35 +1,52 @@
-import { createContext, ReactNode, useState } from "react";
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+} from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../Services/api";
+
 interface IAuthContext {
-	loadingUser: any;
-	dataUser: any;
-	setDataUser: any;
+  loadingUser: () => void;
+  dataUser: iDataUser;
+  setDataUser: Dispatch<SetStateAction<iDataUser>>;
+}
+
+interface iDataUser {
+  email: string;
+  fotoDePerfil: string;
+  password: string;
+  nome: string;
+  id: number;
+  typeUser: "admin" | "dev" | "ong";
 }
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
 interface IAuthChildren {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 export const AuthProvider = ({ children }: IAuthChildren) => {
-	const navigate = useNavigate();
-	const [dataUser, setDataUser] = useState({});
-	const loadingUser = () => {
-		api.get(`/users/${localStorage.userId}`, {
-			headers: {
-				Authorization: `Bearer ${localStorage.token}`,
-			},
-		})
-			.then((res) => {
-				setDataUser(res.data);
-			})
-			.catch(() => navigate("/home"));
-	};
-	return (
-		<AuthContext.Provider value={{ loadingUser, dataUser, setDataUser }}>
-			{children}
-		</AuthContext.Provider>
-	);
+  const navigate = useNavigate();
+  const [dataUser, setDataUser] = useState<iDataUser>({} as iDataUser);
+  const loadingUser = () => {
+    api
+      .get(`/users/${localStorage.userId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.token}`,
+        },
+      })
+      .then((res) => {
+        setDataUser(res.data);
+      })
+      .catch(() => navigate("/home"));
+  };
+  return (
+    <AuthContext.Provider value={{ loadingUser, dataUser, setDataUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
