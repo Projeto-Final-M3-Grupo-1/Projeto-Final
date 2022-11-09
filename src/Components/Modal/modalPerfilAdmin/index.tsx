67b4594e8cb306922da2204c6409bd82ca5dc70a
Gabs Ghidini <gabsghidini@gmail.com>
2022-11-09 02:33:28 -0300

    feat: all perfil close

--- src/Components/Modal/modalPerfilAdmin/index.tsx
+++ src/Components/Modal/modalPerfilAdmin/index.tsx
@@ -7,156 +7,187 @@ import { ButtonCloseModal } from "../../Button/ButtonCloseModal";
 import { StyledBoxModal } from "../ModalLogin/style";
 import { useOutSideClick } from "../../../hooks/useOutSideClick";
 import {
-  StyledModalBody,
-  StyledOngDetails,
-  StyledProjectsRequests,
-  StyledProjectDetails,
-  StyledInfo,
-  StyledContent,
-  StyledButtons,
+	StyledModalBody,
+	StyledOngDetails,
+	StyledProjectsRequests,
+	StyledProjectDetails,
+	StyledInfo,
+	StyledContent,
+	StyledButtons,
 } from "./style";
 import { MdDelete } from "react-icons/md";
 import { UserContext } from "../../../Providers/UserProvider";
 import { ModalCreateTech } from "../ModalCreateTech";
 
 interface iState {
-  length: any;
-  map(arg0: (elem: any) => void): import("react").ReactNode;
-  projects: any;
-  title: string;
-  description: string;
-  id: number;
-  user: number;
-  setProjects: any;
+	length: any;
+	map(arg0: (elem: any) => void): import("react").ReactNode;
+	projects: any;
+	title: string;
+	description: string;
+	id: number;
+	user: number;
+	setProjects: any;
 }
 
 export const ModalPerfilAdmin = () => {
-  const { setShowModal, handleNavigate } = useContext(ProjectsContext);
-  const { handlePerfil } = useContext(UserContext);
+	const { setShowModal, handleNavigate } = useContext(ProjectsContext);
+	const { handlePerfil } = useContext(UserContext);
 
-  const { dataUser } = useContext(AuthContext);
-  const [projects, setProjetcts] = useState([] as unknown as iState);
-  const {
-    handleCreateTech,
-    createTech,
-    requestTechs,
-    techs,
-    requestDeleteTech,
-  } = useContext(UserContext);
+	const { dataUser } = useContext(AuthContext);
+	const [projects, setProjetcts] = useState([] as unknown as iState);
+	const {
+		handleCreateTech,
+		createTech,
+		requestTechs,
+		techs,
+		requestDeleteTech,
+	} = useContext(UserContext);
 
-  const modalRef = useOutSideClick(() => {
-    setShowModal(false);
-  });
+	const modalRef = useOutSideClick(() => {
+		setShowModal(false);
+	});
 
-  useEffect(() => {
-    const getProject = () => {
-      api
-        .get(`/projects`)
-        .then((res) => {
-          setProjetcts(res.data);
-        })
-        .catch((error) => console.log(error));
-    };
-    getProject();
-  }, [dataUser]);
+	useEffect(() => {
+		const getProject = () => {
+			api.get(`/projects`)
+				.then((res) => {
+					setProjetcts(res.data);
+				})
+				.catch((error) => console.log(error));
+		};
+		getProject();
+	}, [dataUser]);
 
-  console.log(projects);
+	console.log(projects);
 
-  return (
-    <>
-      <StyledBoxModal>
-        <StyledModalBody>
-          <ButtonCloseModal callback={() => setShowModal(false)} />
+	return (
+		<>
+			<StyledBoxModal>
+				<StyledModalBody>
+					<ButtonCloseModal callback={handlePerfil} />
 
-          <StyledContent>
-            <StyledOngDetails>
-              <div className="profile">
-                <caption>
-                  <img src={dataUser.fotoDePerfil} alt="" />
-                </caption>
-                <div className="details">
-                  <h3>{dataUser.nome}</h3>
-                  <p>Tech Leader Voluntário</p>
-                </div>
-              </div>
-              <StyledProjectDetails>
-                <StyledInfo>
-                  <p className="label">Nome:</p>
-                  <input className="info" value={dataUser.nome} />
-                </StyledInfo>
+					<StyledContent>
+						<StyledOngDetails>
+							<div className="profile">
+								<caption>
+									<img src={dataUser.fotoDePerfil} alt="" />
+								</caption>
+								<div className="details">
+									<h3>{dataUser.nome}</h3>
+									<p>Tech Leader Voluntário</p>
+								</div>
+							</div>
+							<StyledProjectDetails>
+								<StyledInfo>
+									<p className="label">Nome:</p>
+									<input
+										className="info"
+										value={dataUser.nome}
+									/>
+								</StyledInfo>
 
-                <StyledInfo>
-                  <p className="label">Usuário:</p>
-                  <input className="info" value={dataUser.nome} />
-                </StyledInfo>
+								<StyledInfo>
+									<p className="label">Usuário:</p>
+									<input
+										className="info"
+										value={dataUser.nome}
+									/>
+								</StyledInfo>
 
-                <StyledInfo>
-                  <p className="label">Idade:</p>
-                  <input className="info" placeholder="insira sua idade" />
-                </StyledInfo>
+								<StyledInfo>
+									<p className="label">Idade:</p>
+									<input
+										className="info"
+										placeholder="insira sua idade"
+									/>
+								</StyledInfo>
 
-                <StyledProjectsRequests
-                  style={{ width: "95%", marginTop: "7px" }}
-                >
-                  <h2 className="title">Tecnologias que trabalho</h2>
-                  <div className="projectInfo">
-                    <ul className="techs">
-                      {techs.length ? (
-                        techs.map((element: any) => {
-                          if (element.userId == dataUser.id)
-                            return (
-                              <li key={element.id}>
-                                <h2>{element.tech}</h2>
+								<StyledProjectsRequests
+									style={{ width: "95%", marginTop: "7px" }}
+								>
+									<h2 className="title">
+										Tecnologias que trabalho
+									</h2>
+									<div className="projectInfo">
+										<ul className="techs">
+											{techs.length ? (
+												techs.map((element: any) => {
+													if (
+														element.userId ==
+														dataUser.id
+													)
+														return (
+															<li
+																key={element.id}
+															>
+																<h2>
+																	{
+																		element.tech
+																	}
+																</h2>
 
-                                <MdDelete
-                                  onClick={() => requestDeleteTech(element.id)}
-                                />
-                              </li>
-                            );
-                        })
-                      ) : (
-                        <h2>Cadastre alguma tecnologia</h2>
-                      )}
-                    </ul>
-                  </div>
-                  <StyledButtonCta onClick={handleCreateTech}>
-                    Adicionar tecnologias
-                  </StyledButtonCta>
-                </StyledProjectsRequests>
-              </StyledProjectDetails>
-            </StyledOngDetails>
+																<MdDelete
+																	onClick={() =>
+																		requestDeleteTech(
+																			element.id
+																		)
+																	}
+																/>
+															</li>
+														);
+												})
+											) : (
+												<h2>
+													Cadastre alguma tecnologia
+												</h2>
+											)}
+										</ul>
+									</div>
+									<StyledButtonCta onClick={handleCreateTech}>
+										Adicionar tecnologias
+									</StyledButtonCta>
+								</StyledProjectsRequests>
+							</StyledProjectDetails>
+						</StyledOngDetails>
 
-            <StyledProjectsRequests>
-              <h3 className="title">Solicitações do Projeto</h3>
+						<StyledProjectsRequests>
+							<h3 className="title">Solicitações do Projeto</h3>
 
-              <div className="projectInfo">
-                <h3 className="name">{dataUser.nomeDoResponsavel}</h3>
-                <ul className="projectsInProgress">
-                  {projects.length ? (
-                    projects.map((element) => {
-                      return (
-                        <li>
-                          <p className="title">{element.title}</p>
-                          <button>gerenciar</button>
-                        </li>
-                      );
-                    })
-                  ) : (
-                    <h1>Nenhum projeto</h1>
-                  )}
-                </ul>
-              </div>
-            </StyledProjectsRequests>
-            <StyledButtons>
-              <StyledButtonCadastro>Salvar</StyledButtonCadastro>
-              <StyledButtonCadastro onClick={() => handleNavigate("/home")}>
-                Logout
-              </StyledButtonCadastro>
-            </StyledButtons>
-          </StyledContent>
-        </StyledModalBody>
-      </StyledBoxModal>
-      {createTech && <ModalCreateTech />}
-    </>
-  );
+							<div className="projectInfo">
+								<h3 className="name">
+									{dataUser.nomeDoResponsavel}
+								</h3>
+								<ul className="projectsInProgress">
+									{projects.length ? (
+										projects.map((element) => {
+											return (
+												<li>
+													<p className="title">
+														{element.title}
+													</p>
+													<button>gerenciar</button>
+												</li>
+											);
+										})
+									) : (
+										<h1>Nenhum projeto</h1>
+									)}
+								</ul>
+							</div>
+						</StyledProjectsRequests>
+						<StyledButtons>
+							<StyledButtonCadastro>Salvar</StyledButtonCadastro>
+							<StyledButtonCadastro
+								onClick={() => handleNavigate("/home")}
+							>
+								Logout
+							</StyledButtonCadastro>
+						</StyledButtons>
+					</StyledContent>
+				</StyledModalBody>
+			</StyledBoxModal>
+			{createTech && <ModalCreateTech />}
+		</>
+	);
 };
