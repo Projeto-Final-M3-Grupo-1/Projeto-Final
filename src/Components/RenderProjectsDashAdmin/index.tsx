9e9f49d4e1c2bb8c13cddc008c46b2bcc891e3d5
Rafael Quadros <contatorafaelquadros@gmail.com>
2022-11-09 00:14:53 -0300

    feat/manageProject: first commit

--- src/Components/RenderProjectsDashAdmin/index.tsx
+++ src/Components/RenderProjectsDashAdmin/index.tsx
@@ -1,45 +1,53 @@
-import { useContext, useEffect, useState } from 'react'
-import { ProjectsContext } from '../../Providers/ProjectsProvider';
-import api from '../../Services/api';
-import { StyledDiv } from './style';
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ProjectsContext } from "../../Providers/ProjectsProvider";
+import api from "../../Services/api";
+import { StyledDiv } from "./style";
 
-
-interface iProjects{
+interface iProjects {
     map(arg0: (project: any) => void): import("react").ReactNode;
 }
 
 export const RenderProjectsDashAdmin = () => {
-    const {  requestProjects } =
-    useContext(ProjectsContext);
-    const [projects, setProjects] = useState([] as unknown as iProjects)
+    const { requestProjects, handleManageProject } =
+        useContext(ProjectsContext);
+    const [projects, setProjects] = useState([] as unknown as iProjects);
 
-useEffect(() => {
-    const render = () => {
-      api.get("/projects")
-      .then(res => {
-          setProjects(res.data)
-          console.log(res.data)
-      })
-    }
+    const navigate = useNavigate();
+    useEffect(() => {
+        const render = () => {
+            api.get("/projects").then((res) => {
+                setProjects(res.data);
+                console.log(res.data);
+            });
+        };
 
-    render()
-}, []);
+        render();
+    }, []);
 
-
-return (
-    <StyledDiv>
-        <ul>
-            {projects.map(
-                (project: any) => 
-                    // project.status == "pendings" && (
-                    <li key={project.id}>
-                    <h3>{project.title}</h3>
-                    <button>add</button>
-                </li>
-            // ) 
-            )}        
-        </ul>
-    </StyledDiv>
-
-    )
-}
+    return (
+        <StyledDiv>
+            <ul>
+                {projects.map(
+                    (project: any) => (
+                        // project.status == "pendings" && (
+                        <li key={project.id}>
+                            <h3>{project.title}</h3>
+                            <button
+                                onClick={() =>
+                                    handleManageProject(
+                                        project.id,
+                                        project.ongId
+                                    )
+                                }
+                            >
+                                add
+                            </button>
+                        </li>
+                    )
+                    // )
+                )}
+            </ul>
+        </StyledDiv>
+    );
+};
